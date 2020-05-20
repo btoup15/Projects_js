1e98ee73a9f8bd583b310f4a68682414476f3df9
Ben <ben@toupsonline.com>
2020-05-20 11:48:17 -0500

    test file changes

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log('test commit');
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList.shift();
+groceryList.unshift('popcorn');
+console.log(groceryList);
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
+const pastaIndex = groceryList.indexOf('pasta');
+console.log(pastaIndex)
